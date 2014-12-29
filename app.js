13e88ed3eb6a0f9c055e829349e4dd09c6c70c6b
kirtiEA <kirti@eatads.com>
2014-12-29 19:24:38 +0530

    changes to add lead

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
   $("ul").sortable({
     connectWith: "ul",
     forcePlaceholderSize: true,
@@ -12,14 +13,18 @@ $(document).ready(function () {
       ui.item.removeClass('tilt');
     }
   });
-  /* This is first way to do it where you show a button on clicking add content */
+
+  });
+
+      /* This is first way to do it where you show a button on clicking add content */
   // $(".addContent").click(function(event) {
   //   /* Act on the event */
   //   $(".addContent").addClass("hide");
   //   $("#addnewbtn").addClass("show").removeClass('hide');
   // });
-  /* This is second way to do it where you instantly show a text area.Uncomment next line if that is required behavior. */
-  $(".addContent").on('click', function(){
-    $(".addContent").siblings("ul").find("li:last-child").after('<li class="placeholder"><textarea class="form-control" rows="3" placeholder="Description"></textarea></li>');
-  });
+/* This is second way to do it where you instantly show a text area.Uncomment next line if that is required behavior. */
+  // $(".addContent").on('click', function(){
+  //   console.log("ey champ");
+  //   $(".addContent").siblings("ul").find("li:last-child").after('<li class="placeholder"><textarea class="form-control" rows="3" placeholder="Description"></textarea></li>');
+  // });
 });
