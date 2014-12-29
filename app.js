9accf3a51d41547d968114f0fd85dd672a1d1b9d
kirtiEA <kirti@eatads.com>
2014-12-30 04:40:39 +0530

    fixing bugs

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ $(document).ready(function () {
     connectWith: "ul",
     forcePlaceholderSize: true,
     placeholder: "placeholder",
+    dropOnEmpty: true,
     scroll: true,
     zIndex: 9999,
     start: function (event, ui) {
       ui.item.addClass('tilt');
+       $('ul').css('min-height', '50px');
+        $('ul').sortable('refreshPositions');
     },
     stop: function (event, ui) {
       ui.item.removeClass('tilt');
@@ -16,15 +19,3 @@ $(document).ready(function () {
 
   });
 
-      /* This is first way to do it where you show a button on clicking add content */
-  // $(".addContent").click(function(event) {
-  //   /* Act on the event */
-  //   $(".addContent").addClass("hide");
-  //   $("#addnewbtn").addClass("show").removeClass('hide');
-  // });
-/* This is second way to do it where you instantly show a text area.Uncomment next line if that is required behavior. */
-  // $(".addContent").on('click', function(){
-  //   console.log("ey champ");
-  //   $(".addContent").siblings("ul").find("li:last-child").after('<li class="placeholder"><textarea class="form-control" rows="3" placeholder="Description"></textarea></li>');
-  // });
-});
